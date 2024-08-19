5b9ce6d3d91378d7ffc5bb494f1b7adaaeb35352
samuelsitio26 <112604050+samuelsitio26@users.noreply.github.com>
2024-08-19 23:01:54 +0700

    push

---
+++ 2223-ge-t00-environment-setup-samuelsitio26/t00_01.c
@@ -0,0 +1,11 @@
+// 12S22032 - Samuel Sitio 
+
+
+#include <stdio.h>
+
+int main(int _argv, char **_argc)
+{
+    printf("Hello, world!!");
+
+    return 0;
+}
---
+++ 2223-ge-t00-environment-setup-samuelsitio26/t00_02.c
@@ -0,0 +1,10 @@
+// 12S22032 - Samuel Sitio 
+
+#include <stdio.h>
+
+int main(int _argv, char **_argc)
+{
+   printf("Aloha!");
+
+    return 0;
+}
